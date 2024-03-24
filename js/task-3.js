6eed38477f7428dd3744a48071996fec06e8ea65
25Dmytro <152806456+25Dmytro@users.noreply.github.com>
2024-03-24 15:50:24 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function filterArray(numbers, value) {
+    const selectedArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            selectedArray.push(numbers[i]);
+        }
+    }
+    return selectedArray;
+}
+
+//Перевірка
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
